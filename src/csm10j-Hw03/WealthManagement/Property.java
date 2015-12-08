a7f793d6ba28d3d7ec830083c891462d00be418d
bikenna <brianikenna@gmail.com>
2015-12-07 23:15:08 -0800

    Create Property.java

---
+++ src/csm10j-Hw03/WealthManagement/Property.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InheritanceDesign;
+
+/**
+ *
+ * @author brianobioha
+ */
+public abstract class Property extends Asset {
+    public double debtAmount;
+    public double getDebtAmount(){
+        return debtAmount;
+    }
+}
